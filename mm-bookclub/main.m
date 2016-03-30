f8d257b2400a9466d1980d8a0d745d4152405ac3
Chris Serra <csserra@gmail.com@gmail.com>
2016-03-30 14:30:53 -0700

    Initial Commit

---
+++ mm-bookclub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mm-bookclub
+//
+//  Created by Christopher Serra on 3/30/16.
+//  Copyright © 2016 plugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
